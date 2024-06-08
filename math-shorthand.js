6ec2ec0454a20bf1b8531c3b739eac9e14c4d7dc
shakib8080 <mdshakibulhasan0808@gmail.com>
2024-06-08 16:12:03 +0600

    commit

---
+++ math-shorthand.js
@@ -0,0 +1,18 @@
+// var love = 10;
+// love = love -1;
+// love++;
+// console.log(love);
+
+
+// var price1 = 10;
+// var price2 =  20;
+// price1 +=2;
+// console.log(price1);
+
+var age = 10;
+var age = age + 2;
+age +=10;
+console.log(age);  
+
+
+
--- variable.js
+++ variable.js
@@ -1,4 +1,10 @@
-var age = 120;
-var size = 42;
-var position = 13;
-var name = 'shakib al hasan';
+// var age = 120;
+// var size = 42;
+// var position = 13;
+// var name = 'shakib al hasan';
+var name = "shakib";
+var name = "shik";
+var name = "shohid";
+var allname ="shik + shakib +shohid ";
+
+console.log(allname);
