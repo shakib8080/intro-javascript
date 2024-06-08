26dc67995b843ed97777f7e64aebcf7b1e2a15d9
shakib8080 <mdshakibulhasan0808@gmail.com>
2024-06-08 16:44:01 +0600

    dhf

---
+++ conversion.js
@@ -0,0 +1,9 @@
+var eggprice = '24.25';
+var mangoprice = '26.36';
+var eggprice = parseInt(eggprice);
+console.log(eggprice);
+
+var eggprice = '24.25';
+var mangoprice = '26.36';
+eggprice = parseFloat(mangoprice);
+console.log(eggprice);
